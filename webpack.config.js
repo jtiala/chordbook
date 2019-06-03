c99e54cf74b4215f9ead638e020fc2baadbc8681
Joonas Tiala <joonas.tiala@gmail.com>
2019-06-03 15:31:04 +0300

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const ServiceWorkerWebpackPlugin = require("serviceworker-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
