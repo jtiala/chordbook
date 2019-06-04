17bb9b3b3eab7208ad2d1180bf9c6d8201baa598
Joonas Tiala <joonas.tiala@gmail.com>
2019-06-04 16:50:39 +0300

    Fix more linting errors & add webpack-env types

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,25 @@
-declare let module: any;
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Normalize } from 'styled-normalize';
 
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { Normalize } from "styled-normalize";
+import { SettingsProvider } from './contexts/Settings';
 
-import { SettingsProvider } from "./contexts/Settings";
-
-import App from "./components/App";
+import App from './components/App';
 
 ReactDOM.render(
   <SettingsProvider>
     <Normalize />
     <App />
   </SettingsProvider>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 if (module.hot) {
   module.hot.accept();
 }
 
-if (process.env.NODE_ENV !== "development" && "serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
+if (process.env.NODE_ENV !== 'development' && 'serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
     navigator.serviceWorker.register(`${process.env.PUBLIC_PATH}sw.js`);
   });
 }
