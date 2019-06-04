5fcf7c486526f32987b18059a69d2b4893d6a62d
Joonas Tiala <joonas.tiala@gmail.com>
2019-06-04 12:18:31 +0300

    Add fallback component for authenticated components

--- src/components/AuthenticatedComponent.tsx
+++ src/components/AuthenticatedComponent.tsx
@@ -10,13 +10,18 @@ import Pulse from "./Pulse";
 
 interface IProps {
   children?: React.ReactNode;
+  fallback?: React.ReactNode;
 }
 
-const AuthenticatedComponent: React.SFC<IProps> = ({ children }) => {
+const AuthenticatedComponent: React.SFC<IProps> = ({ children, fallback }) => {
   const [user, loading, error] = useAuthState(auth);
 
   if (user) {
     return <React.Fragment>{children}</React.Fragment>;
+  }
+
+  if (fallback) {
+    return <React.Fragment>{fallback}</React.Fragment>;
   }
 
   return null;
