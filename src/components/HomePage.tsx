651b535bbf52b0cd0ce3b9bac61e0262a60b912a
Joonas Tiala <joonas.tiala@gmail.com>
2019-06-05 18:30:29 +0300

    Modify add a song button

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,17 +1,26 @@
 import * as React from 'react';
+import styled from 'styled-components';
 
 import AuthenticatedComponent from './AuthenticatedComponent';
 import Button from './Button';
 import Page from './Page';
 import SongList from './SongList';
 
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
 const HomePage: React.SFC = () => (
   <Page title="Home">
     <SongList />
     <AuthenticatedComponent>
-      <Button as="Link" variant="primary" to="/songs/new">
-        Add a song
-      </Button>
+      <ButtonContainer>
+        <Button as="Link" to="/songs/new">
+          + Add a song
+        </Button>
+      </ButtonContainer>
     </AuthenticatedComponent>
   </Page>
 );
